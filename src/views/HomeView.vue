1afea7f13daa65f70e669c9a8cd92eda93877506
Norhan Mohammed <norhan@sulfah.com>
2023-10-28 02:08:18 +0200

    feat : init home

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <img
+        alt="V-Movies logo"
+        class="logo"
+        src="@/assets/logo.png"
+        width="130"
+      />
+      <h2>Welcome to {{ appName }} App</h2>
+      <nav>
+        <button @click="goTo('List_Movies')">Movies</button>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
